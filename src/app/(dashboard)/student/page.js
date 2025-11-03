334bc99c01dcd53bc15082cdcf33101fcbb59503
Mahmoud0980 <mahmoud98hh@gmail.com>
2025-11-03 06:02:16 +0300

    Fixed problem

--- src/app/(dashboard)/student/page.js
+++ src/app/(dashboard)/student/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchStudents } from "../../../store/slices/studentsSlice";
