ca2af8f2f96447500b8a53224cebab5e397fe809
Mahmoud0980 <mahmoud98hh@gmail.com>
2026-01-19 11:17:09 +0300

    Fixed problem3

--- src/store/services/baseApi.js
+++ src/store/services/baseApi.js
@@ -1,54 +1,14 @@
-// // src/store/services/baseApi.js
-// import { createApi } from "@reduxjs/toolkit/query/react";
-// import axios from "@/lib/config/axiosConfig";
-// import { tr } from "zod/v4/locales";
-
-// /**
-//  * 🧠 قاعدة مشتركة لكل API مبنية على axiosConfig
-//  */
-// export const axiosBaseQuery =
-//   ({ baseUrl } = { baseUrl: "" }) =>
-//   async ({ url, method, data, body, params, headers }) => {
-//     try {
-//       const result = await axios({
-//         url: baseUrl + url,
-//         method,
-//         data: data ?? body,
-//         params,
-//         headers,
-//       });
-//       return { data: result.data };
-//     } catch (err) {
-//       return {
-//         error: {
-//           status: err.response?.status,
-//           data: err.response?.data || err.message,
-//         },
-//       };
-//     }
-//   };
-
-// /**
-//  * 🧱 إعداد عام موحد لكل APIs
-//  * - refetchOnFocus, reconnect, mount = false
-//  * - الكاش يبقى 5 دقائق (300 ثانية)
-//  */
-// export const baseApiConfig = {
-//   baseQuery: axiosBaseQuery({ baseUrl: "" }),
-//   keepUnusedDataFor: 300,
-//   refetchOnFocus: true,
-//   refetchOnReconnect: true,
-//   refetchOnMountOrArgChange: false,
-// };
 // src/store/services/baseApi.js
+import { createApi } from "@reduxjs/toolkit/query/react";
 import axios from "@/lib/config/axiosConfig";
+import { tr } from "zod/v4/locales";
 
 /**
  * 🧠 قاعدة مشتركة لكل API مبنية على axiosConfig
  */
 export const axiosBaseQuery =
   ({ baseUrl } = { baseUrl: "" }) =>
-  async ({ url, method, data, body, params, headers, responseType }) => {
+  async ({ url, method, data, body, params, headers }) => {
     try {
       const result = await axios({
         url: baseUrl + url,
@@ -56,9 +16,7 @@ export const axiosBaseQuery =
         data: data ?? body,
         params,
         headers,
-        responseType,
       });
-
       return { data: result.data };
     } catch (err) {
       return {
